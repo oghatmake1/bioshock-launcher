c1b9dca2c6edeb4ed6491deb7fb3efeb85c68dd4
oghatmake1 <wmwwwb@gmail.com>
2026-03-09 12:48:02 -0500

    Command line arguments

--- main.gd
+++ main.gd
@@ -3,15 +3,27 @@ extends Node2D
 @onready var player: AudioStreamPlayer2D = $Music
 @onready var back: TextureRect = $Background
 @onready var sel: ItemList = $Games
-var current_track: String = ""
 
-func exit():
-	get_tree().quit()
+@onready var current_track: String = ""
+@onready var args_flags = {} 
 
-func _process(_delta) -> void:
-	var onesel: bool = sel.is_selected(0) or sel.is_selected(1)
-	var twosel: bool = sel.is_selected(2) or sel.is_selected(3)
-	var infsel: bool = sel.is_selected(4)
+func _ready():
+	args_flags = parse_flags() 
+
+func parse_flags() -> Dictionary:
+	var out = {"noimg": false, "nosnd": false}
+	var args = OS.get_cmdline_args()
+	for i in range(args.size()):
+		if args[i] == "-noimg":
+			out.noimg = true
+		elif args[i] == "-nosnd":
+			out.nosnd = true
+	return out
+
+func _process(_delta: float) -> void:
+	var onesel = sel.is_selected(0) or sel.is_selected(1)
+	var twosel = sel.is_selected(2) or sel.is_selected(3)
+	var infsel = sel.is_selected(4)
 
 	if onesel and current_track != "res://Music/1.mp3":
 		play_sound("res://Music/1.mp3")
@@ -24,34 +36,55 @@ func _process(_delta) -> void:
 		change_texture("res://Backgrounds/Infinite.webp")
 
 func play_sound(audio_path: String):
+	if args_flags["nosnd"]:
+		return
+
 	var new_audio_stream: AudioStream = load(audio_path)
+	if not new_audio_stream:
+		push_error("No such audio: " + audio_path)
+		OS.alert("Memory Access Violation\nNo such audio:\n" + audio_path, "Catastrophic Error!")
+		exit()
+
 	player.stream = new_audio_stream
 	player.play()
-	current_track = audio_path  # update current track
-	if not new_audio_stream:
-		push_error("No such audio" + audio_path)
-		OS.alert("Memory Access Violation\nNo such audio.\n" + audio_path, "Catastrophic Error!")
+	current_track = audio_path
+
+func change_texture(new_texture_path: String):
+	if args_flags["noimg"]:
+		return  # skip if no image flag is set
+
+	var new_texture = load(new_texture_path)
+	if not new_texture:
+		push_error("No such image: " + new_texture_path)
+		OS.alert("Memory Access Violation\nNo such image:\n" + new_texture_path, "Catastrophic Error!")
 		exit()
 
-func change_texture(new_texture_path):
-	var new_texture = load(new_texture_path)
 	if new_texture is Texture2D:
 		back.texture = new_texture
-	if not new_texture:
-		push_error("No such image" + new_texture_path)
-		OS.alert("Memory Access Violation\nNo such image\n" + new_texture_path, "Catastrophic Error!")
-		exit()
+''' failed hdr attempt
+		if get_window().get_output_max_linear_value() > 1.1:
+			var shader_material = load("res://Backgrounds/hdr.tres")
+			back.material = shader_material
+			back.material.set_shader_parameter(
+				"mult_map",
+				"res://Backgrounds/multiplicationmaps/" + new_texture_path.get_file()
+			)
+'''
 
 func Play_pressed() -> void:
-	var sup = ["Windows", "Linux", "macOS"]
-	if OS.get_name() in sup:
+	var supported_os = ["Windows", "Linux", "macOS"]
+	if OS.get_name() in supported_os:
 		open_game()
 
 func open_game():
 	const inc = preload("res://Common/constants.gd")
 	var gameslist = inc.gamelist
-	for i in len(gameslist):
-		if $Games.is_selected(i):
+
+	for i in range(len(gameslist)):
+		if sel.is_selected(i):
 			var link = "steam://run/" + str(gameslist[i])
 			OS.shell_open(link)
 			exit()
+
+func exit():
+	get_tree().quit()
